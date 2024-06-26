9681ad3470a91bd6eabfdc6e5e2c30eb340307b8
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-06-26 15:02:11 +0500

    Time: 56 ms (72.47%), Space: 17.5 MB (63.32%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -10,4 +10,12 @@ class Solution:
 
         return ''.join(stack)
 
+        #Explanation
+#Initialize an empty stack: stack = []
+#Iterate through each character of the string:
+#If the stack is not empty and the top of the stack (stack[-1]) is equal to the current character (char), remove the top of the stack (stack.pop()).
+#Otherwise, push the current character onto the stack (stack.append(char)).
+#Build the final string: After processing all characters, the stack will contain the final string without any adjacent duplicates. Convert the stack to a string using ''.join(stack) and return it.
+
+
 
