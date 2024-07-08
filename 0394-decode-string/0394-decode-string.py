fed8f2db5c635da1eb658edbe4e1512b6b7ff509
Muhammad Rayyan <146942220+code-with-anonymous@users.noreply.github.com>
2024-07-08 12:53:34 +0500

    Time: 40 ms (19.25%), Space: 16.5 MB (69.58%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,22 +1,59 @@
+#  class Solution:
+#     def decodeString(self, s: str) -> str:
+#         stack = []
+#         curNum = 0
+#         res = ""
+
+#         for c in s:
+#             if c.isdigit():
+#                 curNum = curNum * 10 + int(c)
+#             elif c == "[":
+#                 stack.append(res)
+#                 stack.append(curNum)
+#                 res = ""
+#                 curNum = 0
+#             elif  c == "]":
+#                 k = stack.pop()
+#                 prev = stack.pop()
+#                 res = prev + k * res
+#             else: 
+#                 res += c
+#         return res
+        
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
-        curNum = 0
-        res = ""
+        num_stack = []
+        string_stack = []
+        k = 0
 
         for c in s:
             if c.isdigit():
-                curNum = curNum * 10 + int(c)
-            elif c == "[":
-                stack.append(res)
-                stack.append(curNum)
-                res = ""
-                curNum = 0
-            elif  c == "]":
-                k = stack.pop()
-                prev = stack.pop()
-                res = prev + k * res
-            else: 
-                res += c
-        return res
+                k = (k * 10) + int(c)
+                continue
+
+            if c == '[':
+                num_stack.append(k)
+                k = 0
+                string_stack.append(c)
+                continue
+
+            if c != ']':
+                string_stack.append(c)
+                continue
+
+            temp = []
+            while string_stack[-1] != '[':
+                temp.insert(0, string_stack.pop())
+
+            # remove the "["
+            string_stack.pop()
+
+            # Get the new string
+            replacement = ''.join(temp) * num_stack.pop()
+
+            # Add it to the stack
+            string_stack.append(replacement)
+
+        result = ''.join(string_stack)
+        return result
         
